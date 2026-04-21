d6ad97139615d0dbccb00a1fb0a6a8ce6978c42f
matheus <abmm1234@hotmail.com>
2026-04-21 15:21:46 -0300

    first commit

---
+++ src/main/java/com/marimo/budget_system/BudgetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.marimo.budget_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSystemApplication.class, args);
+	}
+
+}
